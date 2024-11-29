fdf7ce6f9a9fe5f9f94fbf3285a780905a8b0a22
kris.kensy <kriskensy@proton.me>
2024-11-29 17:47:30 +0100

    Add abstract class AllViewModel. Add command and binding for User table

--- MVVMFirma/ViewModels/AllUsersViewModel.cs
+++ MVVMFirma/ViewModels/AllUsersViewModel.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using MVVMFirma.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using MVVMFirma.Helper;
 
 namespace MVVMFirma.ViewModels
 {
-    public class AllUsersViewModel : WorkspaceViewModel
+    public class AllUsersViewModel : AllViewModel<User>
     {
+        #region Constructor
         public AllUsersViewModel()
+            : base("Users") { }
+        #endregion
+
+        #region Helpers
+        public override void Load()
         {
-            base.DisplayName = "Users";
+            List = new ObservableCollection<User>
+                (
+                    diving4LifeEntities.User.ToList()
+                );
         }
+        #endregion
     }
 }
---
+++ MVVMFirma/ViewModels/AllViewModel.cs
@@ -0,0 +1,64 @@
+﻿using MVVMFirma.Helper;
+using MVVMFirma.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMFirma.ViewModels
+{
+    public abstract class AllViewModel<T> : WorkspaceViewModel
+    {
+        #region DB
+        protected readonly Diving4LifeEntities diving4LifeEntities;
+        #endregion
+
+        #region LoadCommand
+        private BaseCommand _LoadCommand;
+
+        public ICommand LoadCommand
+        {
+            get
+            {
+                if (_LoadCommand == null)
+                    _LoadCommand = new BaseCommand(() => Load());
+                return _LoadCommand;
+            }
+        }
+        #endregion
+
+        #region List
+        private ObservableCollection<T> _List;
+
+        public ObservableCollection<T> List
+        {
+            get
+            {
+                if (_List == null)
+                    Load();
+                return _List;
+            }
+            set
+            {
+                _List = value;
+                OnPropertyChanged(() => List);
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public AllViewModel(String displayName)
+        {
+            diving4LifeEntities = new Diving4LifeEntities();
+            base.DisplayName = displayName;
+        }
+        #endregion
+
+        #region Helpers
+        public abstract void Load();
+        #endregion
+    }
+}
