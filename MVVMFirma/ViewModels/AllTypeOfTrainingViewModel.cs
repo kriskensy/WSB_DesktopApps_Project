8249c43e8d5c766b034747cc742ec4c4c714e3ae
kris.kensy <kriskensy@proton.me>
2024-12-11 20:10:31 +0100

    Add displaying TypeOfTraining table

--- MVVMFirma/ViewModels/AllTypeOfTrainingViewModel.cs
+++ MVVMFirma/ViewModels/AllTypeOfTrainingViewModel.cs
@@ -1,16 +1,28 @@
-﻿using System;
+﻿using MVVMFirma.Models.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVMFirma.ViewModels
 {
-    public class AllTypeOfTrainingViewModel : WorkspaceViewModel
+    public class AllTypeOfTrainingViewModel : AllViewModel<TypeOfTraining>
     {
+        #region Constructor
         public AllTypeOfTrainingViewModel()
+            : base ("Training Types") { }
+        #endregion
+
+        #region Helpers
+        public override void Load()
         {
-            base.DisplayName = "Training Types";
+            List = new ObservableCollection<TypeOfTraining>
+                (
+                diving4LifeEntities.TypeOfTraining.ToList()
+                );
         }
+        #endregion
     }
 }
